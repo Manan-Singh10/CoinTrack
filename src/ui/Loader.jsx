2ebdf1b418a6b6657e98fe4bde61e907bb7ea974
Manan Singh <mananmessi491@gmail.com>
2025-04-20 12:53:45 +0530

    loader fix

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="flex items-center justify-center h-full w-full">
-      <img src="../public/loader.svg" alt="Loading..." className="w-30 h-30" />
+      <img src="/loader.svg" alt="Loading..." className="w-30 h-30" />
     </div>
   );
 };
